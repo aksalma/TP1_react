c51acfde5942ca5e78aade4688df3afb19ec76a5
aksalma <akhdimsalma01@gmail.com>
2023-11-06 18:42:15 +0100

    message

--- src/Composants/calcul.js
+++ src/Composants/calcul.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-
+import './calcul.css'
 
 export default function Calc() {
 
@@ -30,39 +30,42 @@ setExp(newval);
 }
 
  return (
-    <div style={{ backgroundColor: 'black', color: 'white', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <div className='container w-25 p-5 ' style={{ backgroundColor: '#ccc',borderRadius: '10px'}}>
-            <div className='d-flex justify-content-between align-items-center'>
-         <input value={exp} type="text" className='form-control bg-light text-dark' />
-         <input onClick={clear} type="button" value="C" className='btn btn-danger '/>
-         </div>
+    <div className='container' >
+        <div className='wrapper'>
+        <div className='head'>
+  <div className='screen'>
+    <input value={exp} type="text" className='form-control ' style={{ flex: '75%' }} />
+    <input onClick={clear} type="button" value="C" className='btn clear' style={{ flex: '25%' }} />
+  </div>
+</div>
+
        
-       <div className='d-flex justify-content-between align-items-center w-100  p-1 '>
-      <input onClick={(e)=>tapecar(e)} type="button" value="1" className='w-25 p-2 mx-1 btn btn-secondary' />
-      <input onClick={(e)=>tapecar(e)} type="button" value="2" className='w-25 p-2 mx-1 btn btn-secondary' />
-      <input onClick={(e)=>tapecar(e)} type="button" value="3" className=' w-25 p-2 mx-1 btn btn-secondary' />
-      <input onClick={(e)=>tapecar(e)} type="button" value="+" className=' w-25 p-2 mx-1 btn btn-primary' />
+       <div className='d-flex justify-content-between w-100  p-1 '>
+      <input onClick={(e)=>tapecar(e)} type="button" value="1" className='w-25 p-2 mx-1 btn light-gray ' />
+      <input onClick={(e)=>tapecar(e)} type="button" value="2" className='w-25 p-2 mx-1 btn light-gray' />
+      <input onClick={(e)=>tapecar(e)} type="button" value="3" className=' w-25 p-2 mx-1 btn light-gray' />
+      <input onClick={(e)=>tapecar(e)} type="button" value="+" className=' w-25 p-2 mx-1 btn orange' />
       </div>
      
-      <div className='d-flex justify-content-between align-items-center w-100 p-1 '>
-      <input onClick={(e)=>tapecar(e)} type="button" value="4" className='w-25 p-2 mx-1 btn btn-secondary' />
-      <input onClick={(e)=>tapecar(e)} type="button" value="5" className=' w-25 p-2 mx- 1 btn btn-secondary' />
-      <input onClick={(e)=>tapecar(e)} type="button" value="6" className='w-25 p-2 mx-1 btn btn-secondary' />
-      <input onClick={(e)=>tapecar(e)} type="button" value="-" className='w-25 p-2 mx-1 btn btn-primary' />
+      <div className='d-flex justify-content-between w-100 p-1 '>
+      <input onClick={(e)=>tapecar(e)} type="button" value="4" className='w-25 p-2 mx-1 btn light-gray ' />
+      <input onClick={(e)=>tapecar(e)} type="button" value="5" className=' w-25 p-2 mx- 1 btn light-gray' />
+      <input onClick={(e)=>tapecar(e)} type="button" value="6" className='w-25 p-2 mx-1 btn light-gray' />
+      <input onClick={(e)=>tapecar(e)} type="button" value="-" className='w-25 p-2 mx-1 btn orange' />
       </div>
       
-      <div className='d-flex justify-content-between align-items-center w-100 p-1 '>
-      <input onClick={(e)=>tapecar(e)} type="button" value="7" className='w-25 p-2 mx-1 btn btn-secondary' />
-      <input onClick={(e)=>tapecar(e)} type="button" value="8" className=' w-25 p-2 mx-1 btn btn-secondary' />
-      <input onClick={(e)=>tapecar(e)} type="button" value="9" className=' w-25 p-2 mx-1 btn btn-secondary' />
-      <input onClick={(e)=>tapecar(e)} type="button" value="*" className=' w-25 p-2 mx-1 btn btn-primary' />
+      <div className='d-flex justify-content-between  w-100 p-1 '>
+      <input onClick={(e)=>tapecar(e)} type="button" value="7" className='w-25 p-2 mx-1 btn light-gray' />
+      <input onClick={(e)=>tapecar(e)} type="button" value="8" className=' w-25 p-2 mx-1 btn light-gray' />
+      <input onClick={(e)=>tapecar(e)} type="button" value="9" className=' w-25 p-2 mx-1 btn light-gray' />
+      <input onClick={(e)=>tapecar(e)} type="button" value="*" className=' w-25 p-2 mx-1 btn orange' />
       </div>
     
-      <div className='d-flex justify-content-between align-items-center w-100 p-1 '>
-      <input onClick={(e)=>tapecar(e)} type="button" value="." className='w-25 mx-1 p-2 btn btn-secondary' />
-      <input onClick={(e)=>tapecar(e)} type="button" value="0" className=' w-25 p-2 mx-1 btn btn-secondary' />
-      <input onClick={result} type="button" value="=" className=' w-25 p-2 mx-1 btn btn-secondary' />
-      <input onClick={(e)=>tapecar(e)} type="button" value="/" className='p-2 w-25 mx-1 btn btn-primary' />
+      <div className='d-flex justify-content-between  w-100 p-1 '>
+      <input onClick={(e)=>tapecar(e)} type="button" value="." className='w-25 mx-1 p-2 btn light-gray' />
+      <input onClick={(e)=>tapecar(e)} type="button" value="0" className=' w-25 p-2 mx-1 btn light-gray' />
+      <input onClick={result} type="button" value="=" className=' w-25 p-2 mx-1 btn light-gray' />
+      <input onClick={(e)=>tapecar(e)} type="button" value="/" className='p-2 w-25 mx-1 btn orange' />
       </div>
       
       </div>
